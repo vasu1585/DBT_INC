ae7316c52c107b40b075698230df1b7e7dc05e60
Srinivas Vobulasetty <vasu1585@gmail.com>
2021-01-24 01:24:08 -0600

    DBT_INC

--- models/dbt_watermark/WATERMARK.sql
+++ models/dbt_watermark/WATERMARK.sql
@@ -7,7 +7,7 @@ SELECT
     MD.SENSOR_IDX as SNSR_IDX,
     MAX(METER.EVENT_TS)
     END  AS MAX_TS
-    FROM   {{ source('dbt_EVENTS_src', 'EVENTS') }} METER LEFT JOIN {{ source('dbt_watermark', 'MASTERDATA') }} MD
+    FROM   {{ source('dbt_events_src', 'EVENTS') }} METER LEFT JOIN {{ source('dbt_watermark', 'MASTERDATA') }} MD
 
     on MD.SENSOR_IDX = METER.SENSOR_IDX
     GROUP BY METER.METER_NUMBER_INDEX
